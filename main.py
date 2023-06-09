d56cb6e39223c971970e44acb5ac3c6f40ab8a43
mahdi <mahdimanoun.15@gmail.com>
2023-06-09 22:41:13 +0300

    Errors Edited

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, HTTPException
 from typing import List
 from uuid import UUID
-from models import UserInfo, UpdateUserInfo
+from models import UserInfo, UpdateUserInfo, Birthday, check_Birthday
+import datetime
 
 
 app = FastAPI()
@@ -10,7 +11,7 @@ app = FastAPI()
 db: List[UserInfo] = []
 
 
-@app.get("/facebook.com")
+@app.get("/")
 def fetch_all_users():
     return db
 
@@ -24,9 +25,17 @@ def fetch_users(username: str):
             detail=f"{username} not found"
         )
 
-@app.post("/facebook.com/sign_up")
+@app.post("/api/auth/sign_up")
 def create_acc(user: UserInfo):
+    current_year = datetime.datetime.now().year
+    for existing_user in db:
+        if existing_user.email == user.email:
+            return f"{user.email} is already in use"
+    check_Birthday(user.birthdate)
     db.append(user)
+    return f"Your date of birth is: {user.birthdate.year}-{user.birthdate.month}-{user.birthdate.day}! Your Age is {current_year-user.birthdate.year}"
+
+
 
 
 @app.put("/facebook.com/{user_id}")
@@ -41,8 +50,8 @@ def edit_info(update_user: UpdateUserInfo, user_id: UUID):
                 user.username = update_user.username
             if user.surname != update_user.surname:
                 user.surname = update_user.surname
-            if user.age != update_user.age:
-                user.age = update_user.age
+            if user.birthdate != update_user.birthdate:
+                user.birthdate = update_user.birthdate
             if user.email != update_user.email:
                 user.email = update_user.email
             if user.password != update_user.password:
@@ -64,3 +73,4 @@ def delete_user(user_id: UUID):
             status_code=404,
             detail=f"User with id: {user_id} does not found."
         )
+
--- models.py
+++ models.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
 from uuid import UUID
-from typing import Optional
+from typing import Optional, List
+import datetime
+
+
+
+class Birthday(BaseModel):
+    year: int
+    month: int
+    day: int
 
 class UserInfo(BaseModel):
     id: UUID
@@ -9,16 +17,46 @@ class UserInfo(BaseModel):
     last_name: str
     username: str
     surname: Optional[str]
-    age: int
+    birthdate: Birthday = Field(..., description="Date of birth")
     email: str
     password: str
 
 
 class UpdateUserInfo(BaseModel):
-    first_name: str
-    last_name: str
-    username: str
+    first_name: Optional[str]
+    last_name: Optional[str]
+    username: Optional[str]
     surname: Optional[str]
-    age: int
-    email: str
-    password: str
+    birthdate: Optional[Birthday] = Field(..., description="Date of birth")
+    email: Optional[str]
+    password: Optional[str]
+
+
+def check_Birthday(birthdate: Birthday):
+    current_year = datetime.datetime.now().year
+
+    if birthdate.year < 1940 or birthdate.year > current_year:
+        raise HTTPException(
+            status_code=400,
+            detail="Enter a valid year"
+        )
+
+    if birthdate.year > current_year - 18:
+        raise HTTPException(
+            status_code=400,
+            detail="Your age is less than 18"
+        )
+
+    if birthdate.month < 1 or birthdate.month > 12:
+        raise HTTPException(
+            status_code=400,
+            detail="Enter a valid month"
+        )
+
+    if birthdate.day < 1 or birthdate.day > 31:
+        raise HTTPException(
+            status_code=400,
+            detail="Enter a valid day"
+        )
+
+
